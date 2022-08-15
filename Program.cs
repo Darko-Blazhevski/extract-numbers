449799749c2a5443bee21784b0b6c5f78b314988
Darko Blazhevski <darkoblazhevski@outlook.com>
2022-08-15 14:42:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace extract_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imput;
+            do
+            {
+                imput = Console.ReadLine();
+            } while (imput == string.Empty);
+
+            string inter;
+            inter = ekstrakcija(imput);
+            Console.WriteLine(inter);
+
+        }
+
+        private static string ekstrakcija(string imput)
+        {
+            string inter = string.Empty;
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(imput);
+            foreach (var item in asciiBytes)
+            {
+                if (item <= 57 && item >= 47)
+                {
+                    inter = inter + Convert.ToChar(item);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return inter;
+        }
+    }
+}
